df242e58920526d4a14ef4dc9901266627183c45
limana-1247 <analauramasca@gmail.com>
2022-10-21 18:25:48 -0300

    primeiro commit

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const alunos = [
 {id:1, name: "Ana",Bim1: 5.0, Bim2: 4.0, Bim3: 6.0,Bim4:7.0, media:10.0},
 {id:2, name: "Flávia",Bim1: 3.0, Bim2: 6.0, Bim3: 9.0,Bim4:4.0, media:7.0},
 {id:3, name: "Júlio",Bim1: 10.0, Bim2: 10.0, Bim3: 10.0,Bim4:10.0, media:10.0},
-{id:4, name: "Pedro",Bim1: 8.0, Bim2:8.0, Bim3: 8.0,Bim4:7.0, media:8.0},
 ]
 
 alunos.forEach(notas =>{
